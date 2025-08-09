7ca85ceb112be625346f0f7c4b8b1572d22b8629
Aravind <aravind19112003@gmail.com>
2025-08-09 21:45:02 +0530

    Add chunker

--- backend/chunker.py
+++ backend/chunker.py
@@ -1,2 +1,25 @@
+import nltk
+nltk.download('punkt')
+
 def split_text_into_chunks(text, chunk_size=500, overlap=100):
-    pass
+    
+    sentences = nltk.sent_tokenize(text)
+    out = []
+    curr = ""
+    for sentence in sentences:
+        if(len(curr) + len(sentence) <= chunk_size):
+            curr += " " + sentence
+        else:
+            out.append(curr.strip())
+            # start a new chunk
+            curr = " ".join(
+                # we can take last few sentences for overlap
+                sentences[
+                    max(0, sentences.index(sentence) - 2) : sentences.index(sentence) + 1
+                ]
+            )
+    
+    if curr.strip():
+        out.append(curr.strip())
+
+    return out
